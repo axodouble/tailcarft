050a2c598baa1cac087bfc9e8823615b78638a49
Jasper (Axodouble) V. <software@jas.pe>
2026-09-15 09:46:12 +0000

    mod: add invite wrapping/validation (Invites)

---
+++ mod/src/test/java/com/tailscale/mclink/InvitesTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2026, Jasper (Axodouble) V. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package com.tailscale.mclink;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class InvitesTest {
+    static final String WRAPPED_TC_TEST = "mcl1_eyJ2ZXJzaW9uIjoxLCJ0YWlsY2F0IjoidGNfdGVzdCJ9";
+
+    @Test
+    void wrapsBareTokenByteExact() {
+        assertEquals(WRAPPED_TC_TEST, Invites.wrap("tc_test"));
+    }
+
+    @Test
+    void normalizeWrapsBareToken() {
+        assertEquals(WRAPPED_TC_TEST, Invites.normalize("tc_test"));
+    }
+
+    @Test
+    void normalizePassesThroughEnvelope() {
+        assertEquals("mcl1_abc", Invites.normalize("mcl1_abc"));
+        assertEquals("mcl1_abc", Invites.normalize("  mcl1_abc  "));
+    }
+
+    @Test
+    void normalizeRejectsGarbage() {
+        assertNull(Invites.normalize(null));
+        assertNull(Invites.normalize(""));
+        assertNull(Invites.normalize("   "));
+        assertNull(Invites.normalize("mcl2_abc"));
+        assertNull(Invites.normalize("tc"));
+        assertNull(Invites.normalize("tc_bad char"));
+        assertNull(Invites.normalize("tc\"quote"));
+    }
+
+    @Test
+    void wrapRejectsNonTokens() {
+        assertThrows(IllegalArgumentException.class, () -> Invites.wrap("mcl1_abc"));
+        assertThrows(IllegalArgumentException.class, () -> Invites.wrap("x"));
+        assertThrows(IllegalArgumentException.class, () -> Invites.wrap("tc\"quote"));
+    }
+}
